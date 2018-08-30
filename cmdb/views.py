1cf8bf76b8c2c5bb68fab75c54886f2da9c19aeb
xiafanli <xiafanli1987@163.com>
2018-08-30 15:01:10 +0800

    ok

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpRequest,HttpResponse,HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render_to_response("index.html")
