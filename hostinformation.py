57bc71678f8cbbf266f08a14a9084f73e0b90f60
xiafanli <xiafanli1987@163.com>
2018-09-11 22:40:18 +0800

    ok

--- hostinformation.py
+++ hostinformation.py
@@ -2,6 +2,7 @@ import  requests
 import subprocess
 import json
 import re
+from decimal import *
 
 
 ip_exp = re.compile(u'10(\.(2(5[0-5]{1}|[0-4]\d{1})|[0-1]?\d{1,2})){3}')
@@ -15,38 +16,39 @@ def runCommand(cmd):
 
 def main():
     # Frist get all server info value
-    hostname = runCommand('hostname')
+    hostname = runCommand('hostname').strip()
     ip = ip_exp.search(runCommand('ifconfig')).group()
-    cpunum = runCommand('more /proc/cpuinfo  |grep process |wc -l')
-    memnum = int(runCommand("more /proc/meminfo  |grep MemTotal |awk '{print $2}'"))/1024/1024
-    serial = runCommand('dmidecode -t 1 |grep Serial Number')
-    if serial.startswith('VMware'):
+    cpunum = runCommand('more /proc/cpuinfo  |grep process |wc -l').strip()
+    memnum = Decimal(int(runCommand("more /proc/meminfo  |grep MemTotal |awk '{print $2}'"))/1024/1024)
+    serial = runCommand("dmidecode -t 1 |grep 'Serial Number'|awk -F: '{print $2}'").strip()
+    if serial.startswith('VMware') or serial == '0':
         server_type = 'virtual'
-        serial = ''
+        data = {
+             "hostname": hostname,
+             "ipaddress": ip,
+             "numcpu": cpunum,
+             "nummem": str(memnum),
+             "server_type": server_type}
     else:
         server_type = 'physical'
-
-    data = {
-        "hostname": hostname,
-        "ipaddress": ip,
-        "serial": serial,
-        "rack_id": "",
-        "numcpu": cpunum,
-        "nummem": memnum,
-        "server_type": server_type
-    }
+        data = {
+             "hostname": hostname,
+             "ipaddress": ip,
+             "serial": serial,
+             "numcpu": cpunum,
+             "nummem": str(memnum),
+             "server_type": server_type}
     print(data)
     # fetch from ip
     url = 'http://10.0.0.254:8888/apid3/'
     request = requests.get(url +  'fetch/?ipaddress='  + ip)
-    print(request.status_code)
     if len(json.loads(request.content)) == 0:
+        print("step1")
         requests.post(url, data=json.dumps(data), headers=headers)
     else:
-        print
+        print("step 2")
         pk = json.loads(request.content)[0]['id']
-        requests.post(url + pk, headers=headers, data=json.dumps(data))
-
+        requests.put(url +  str(pk) + "/", headers=headers, data=json.dumps(data))
 
 if __name__ == "__main__":
     main()
